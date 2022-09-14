4dc5a4e34b2ca288a17a6d12929f5a5c4cc48dce
Nadia <nadia.aina@weareadaptive.com>
2022-09-14 11:07:30 +0100

    feat: toggle between start and stop styles on button click

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-function startTimer() {
-  console.log("Started stopwatch");
+function toggleTimer() {
+  const timerControl = document.querySelector("#stopwatch-control");
+  const controlBtn = document.querySelector("#stopwatch-control-btn");
+
+  if (timerControl.classList.contains("start")) {
+    timerControl.classList.remove("start");
+    timerControl.classList.add("stop");
+
+    controlBtn.innerText = "Stop";
+  } else if (timerControl.classList.contains("stop")) {
+    timerControl.classList.remove("stop");
+    timerControl.classList.add("start");
+
+    controlBtn.innerText = "Start";
+  }
 }
