8ad2471e6ed62fe497a31d0c7c32fc658b565409
Nadia <nadia.aina@weareadaptive.com>
2022-09-15 16:03:04 +0100

    feat: implement the stopwatch counter

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const completedLaps = ["00:00.00"];
+let timerID = null;
+let startTime, lapTime, currentTime;
 
 function toggleTimer() {
   const timerControl = document.querySelector("#stopwatch-control");
@@ -9,12 +11,42 @@ function toggleTimer() {
     timerControl.classList.add("stop");
 
     controlBtn.innerText = "Stop";
+
+    startTime = new Date();
+    timerID = setInterval(startStopwatch, 10);
   } else if (timerControl.classList.contains("stop")) {
     timerControl.classList.remove("stop");
     timerControl.classList.add("start");
 
     controlBtn.innerText = "Start";
+
+    clearInterval(timerID);
   }
+}
+
+function startStopwatch() {
+  const timerText = document.querySelector(".timer-display>span");
+  currentTime = new Date();
+
+  const timeLapsedInSeconds = currentTime.getTime() - startTime.getTime();
+
+  const timeLapsed = new Date(timeLapsedInSeconds);
+
+  let millisecs = timeLapsed.getMilliseconds();
+  if (!millisecs || millisecs < 10) {
+    millisecs = "00";
+  } else if (millisecs < 99) {
+    millisecs = "0" + String(millisecs).slice(0, 1);
+  } else if (millisecs % 100 === 0) {
+    millisecs = String(millisecs).slice(0, 1) + "0";
+  } else {
+    millisecs = String(millisecs).slice(0, 2);
+  }
+
+  timerText.innerText = `${timeLapsed.toLocaleTimeString("en", {
+    second: "2-digit",
+    minute: "2-digit",
+  })}.${millisecs}`;
 }
 
 function newLap() {
@@ -27,7 +59,6 @@ function newLap() {
   for (const lap of lapDivs) {
     if (!lap.innerHTML) {
       lap.innerHTML = lapMarkup;
-      isDisplayFull = false;
       break;
     }
   }
