bdbc3f03f7afaae2a624e6cd0d4b33691ab182db
Nadia <nadia.aina@weareadaptive.com>
2022-09-22 13:38:20 +0100

    refactor: replace if/else statements in formatTime with padStart and maths operations

--- utils.js
+++ utils.js
@@ -6,23 +6,15 @@ const toggleIsStopwatchRunning = () =>
 
 const formatTime = (time) => {
   let formattedTime = null;
-  let formattedMillisecs = null;
-  const millisecs = time % 1000; // Gets the reminder of time / 1000
 
-  if (!millisecs || millisecs < 10) {
-    formattedMillisecs = millisecs;
-  } else if (millisecs < 99) {
-    formattedMillisecs = "0" + String(millisecs).slice(0, 1);
-  } else if (millisecs % 100 === 0) {
-    formattedMillisecs = String(millisecs).slice(0, 1) + "0";
-  } else {
-    formattedMillisecs = String(millisecs).slice(0, 2);
-  }
+  let centiseconds = Math.floor((time % 1000) / 10).toString();
+
+  centiseconds = centiseconds.padStart(2, "0");
 
   formattedTime = `${new Date(time).toLocaleTimeString("en", {
     second: "2-digit",
     minute: "2-digit",
-  })}.${formattedMillisecs}`;
+  })}.${centiseconds}`;
 
   return formattedTime;
 };
