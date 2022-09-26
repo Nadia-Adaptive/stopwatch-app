9173baca9abf5e65869fe48909601d5170c6e3f8
Nadia <nadia.aina@weareadaptive.com>
2022-09-26 10:54:23 +0100

    fix: reset best and worst times on stopwatch restart

--- main.js
+++ main.js
@@ -97,4 +97,6 @@ const resetStopwatchTimes = (state) => {
   for (const property in state) {
     state[property] = null;
   }
+  state.bestLapTime = Number.MAX_SAFE_INTEGER;
+  state.worstLapTime = Number.MIN_SAFE_INTEGER;
 };
